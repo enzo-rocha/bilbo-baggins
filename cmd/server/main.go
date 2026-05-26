0a836d5d0a51758cc6c9187ca66925df529caa4e
enzoroch1605 <enzoroch1605@gmail.com>
2026-05-25 23:36:34 -0300

    Redesign homepage game hub

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	httpdelivery "bilbo-baggins/internal/delivery/http"
 	"bilbo-baggins/internal/infra/web"
@@ -10,14 +11,19 @@ import (
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	homeRepository := web.NewHomePageRepository()
 	showHomePage := usecase.NewShowHomePage(homeRepository)
 	homeHandler := httpdelivery.NewHomeHandler(showHomePage)
 
 	router := httpdelivery.NewRouter(homeHandler, web.StaticFileSystem())
 
-	log.Println("server running at http://localhost:8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	log.Printf("server running at http://localhost:%s", port)
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatal(err)
 	}
 }
